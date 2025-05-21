7a73ec749fe1523517f71ad6150792cbe1bd9850
KanataIwata <konkuro5696@gmail.com>
2025-05-21 15:46:45 +0900

    5/21

---
+++ MortEC/src/main/java/com/example/MortEC/MortEcApplication.java
@@ -0,0 +1,13 @@
+package com.example.MortEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortEcApplication.class, args);
+	}
+
+}
---
+++ MortEC/src/test/java/com/example/MortEC/MortEcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MortEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MortEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
